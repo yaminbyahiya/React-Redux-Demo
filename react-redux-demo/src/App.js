26322d04b745e92d9f09155a2d5eb5587a27427e
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-19 18:42:45 +0600

    Redux Store on App using Provider

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react';
+import store from './redux/cakes/store';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer></CakeContainer>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer></CakeContainer>
+      </div>
+    </Provider>
   );
 }
 
